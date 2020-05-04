4dfd1c224a97498e55491fae8b207c1e85db49c5
Yu Wang <yuwang4@andrew.cmu.edu>
2020-05-03 20:55:39 -0400

    Finish CUDA framework

--- bench/cuda_bench.cpp
+++ bench/cuda_bench.cpp
@@ -9,15 +9,24 @@
 #include "gru.h"
 #include "lstm.h"
 
+#include <chrono>
+
 static void BM_CUDA_MatDot_100(benchmark::State& state) {
     Matrix m1(10, 10);
     Matrix m2(10, 10);
     for (auto _ : state) {
+        auto start = std::chrono::high_resolution_clock::now();
         m1.dot(m2);
+        auto end = std::chrono::high_resolution_clock::now();
+        auto elapsed_seconds = 
+            std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
+
+        state.SetIterationTime(elapsed_seconds.count());
     }
+
 }
 
 // Register the function as a benchmark
-BENCHMARK(BM_CUDA_MatDot_100);
+BENCHMARK(BM_CUDA_MatDot_100)->UseManualTime();
 
 BENCHMARK_MAIN();
